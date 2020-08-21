5d9dfe7bdc92ebee26515ef5e29cb03b1f8da38b
Joshua Levine <joshlevine@berkeley.edu>
2020-08-20 19:00:51 -0700

    Initial commit

---
+++ app/src/main/java/com/example/iluriadashboard/FirstFragment.kt
@@ -0,0 +1,79 @@
+package com.example.iluriadashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.jjoe64.graphview.GraphView
+import com.jjoe64.graphview.helper.StaticLabelsFormatter
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.LineGraphSeries
+import java.util.*
+
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+        createGraph()
+    }
+
+    fun createGraph() {
+
+        // generate Dates
+        // generate Dates
+        val calendar: Calendar = Calendar.getInstance()
+        val d1: Date = calendar.getTime()
+        calendar.add(Calendar.DATE, 1)
+        val d2: Date = calendar.getTime()
+        calendar.add(Calendar.DATE, 1)
+        val d3: Date = calendar.getTime()
+
+
+
+        val graph = getView()?.findViewById<View>(R.id.graph) as GraphView
+        val series: LineGraphSeries<DataPoint> = LineGraphSeries(
+            arrayOf(
+                DataPoint(0.0, 1.0),
+                DataPoint(1.0, 5.0),
+                DataPoint(2.0, 3.0)
+            )
+        )
+        series.setDrawDataPoints(true)
+        series.setDataPointsRadius(10.0F);
+        graph.addSeries(series)
+
+        // set manual x bounds to have nice steps
+        graph.getViewport().setMinX(0.0);
+        graph.getViewport().setMaxX(24.0);
+        graph.getViewport().setXAxisBoundsManual(true);
+
+        graph.getViewport().setMinY(0.0);
+        graph.getViewport().setMaxY(20.0);
+        graph.getViewport().setYAxisBoundsManual(true);
+        // set date label formatter
+        val staticLabelsFormatter = StaticLabelsFormatter(graph)
+        staticLabelsFormatter.setHorizontalLabels(arrayOf("12 AM", "6", "12 PM", "6", ""))
+        //staticLabelsFormatter.setVerticalLabels(arrayOf("low", "middle", "high"))
+        graph.gridLabelRenderer.labelFormatter = staticLabelsFormatter
+
+    }
+}
---
+++ app/src/main/java/com/example/iluriadashboard/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.iluriadashboard
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.jjoe64.graphview.GraphView
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.LineGraphSeries
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+        //createGraph()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun createGraph() {
+        val graph = findViewById<View>(R.id.graph) as GraphView
+        val series: LineGraphSeries<DataPoint> = LineGraphSeries(
+            arrayOf(
+                DataPoint(0.0, 1.0),
+                DataPoint(1.0, 5.0),
+                DataPoint(2.0, 3.0),
+                DataPoint(3.0, 2.0),
+                DataPoint(4.0, 6.0)
+            )
+        )
+        graph.addSeries(series)
+
+
+    }
+}
